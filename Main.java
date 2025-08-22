e225cf481790479f8ce6d133956b4d75098c46b2
Maharajan07 <maharajan03007@gmail.com>
2025-08-22 19:58:07 +0530

    String Builder

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java"); 
+        sb.append(" Programming");  // "Java Programming"
+        sb.delete(4, 6);    // 4-inclusive, 6-exclusive
+        System.out.print(sb); 
+    } 
+}
+
+//  Javarogramming
